56f2bc467c1b472f6b585c4015cb268f7c861ddf
Ayush <122601890+Ayushkumar890@users.noreply.github.com>
2023-06-17 22:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,79 @@
+// count the number of drum by using length property
+
 var numberofdrum = document.querySelectorAll(".drum").length
 
-// function handelclick(){
-//   var audio = new Audio('sounds/snare.mp3');
-//   audio.play();
+// create function of key press and sound
+
+function keysound(key){
+
   
-// }
+  switch (key) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+      
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+  
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+  
+    case "l":
+      var kickbass = new Audio('sounds/kick-bass.mp3');
+      kickbass.play();
+      break;
+  
+    default:
+      console.log()
+      break;
+  }
+}
+
+// location of which key is press 
+// we write the keypress instead of keydown
+document.addEventListener("keypress",function(event){
+  keysound(event.key);
+  buttonAnimation(event.key);
+
+})
+
+
+
 
 for(var i = 0;i<numberofdrum;i++){
-  document.querySelectorAll('.drum')[i].addEventListener("click",function(){
-    // var audio = new Audio('sounds/snare.mp3');
-    // audio.play();
-    // this.style.color = "white";
+  document.querySelectorAll('.drum')[i].addEventListener("keypress",function(){
+  
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio('sounds/tom-1.mp3');
-        audio.play();
-        break;
-      case "a":
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        audio.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio('sounds/tom-3.mp3');
-        audio.play();
-        break;
-      
-      case "d":
-        var tom4 = new Audio('sounds/tom-4.mp3');
-        audio.play();
-        break;
-      
-      case "j":
-        var snare = new Audio('sounds/snare.mp3');
-        audio.play();
-        break;
-    
-      case "k":
-        var crash = new Audio('sounds/crash.mp3');
-        audio.play();
-        break;
-    
-      case "l":
-        var kickbass = new Audio('sounds/kick-bass.mp3');
-        audio.play();
-        break;
-    
-      default:
-        console.log()
-        break;
-    }
+    keysound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
+}
 
+function buttonAnimation(currentkey){
+  var activekey = document.querySelector("."+currentkey);
+
+  activekey.classList.add("pressed");
+  setTimeout(function(){
+    activekey.classList.remove("pressed");
+  },200);
 }
